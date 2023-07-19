3529d8f3cc8fd5ee673e2660ce859dab6f98e65a
Maks Rybalchenko <maximmro@gmail.com>
2023-07-20 00:14:30 +0300

    create readme.md; add venv creating to shell scripts;

--- run_vikingage.sh
+++ run_vikingage.sh
@@ -1,2 +1,13 @@
+# create venv
+python3 -m venv venv
+
+# activate venv
+source venv/bin/activate
+
+# install requirements
+pip install -r requirements.txt
+
 curr_datetime=$(date +%Y_%m_%d-%H%M%S)
 scrapy crawl vikingage -O log/vikingage_output_"${curr_datetime}".json --logfile log/vikingage_"${curr_datetime}".log --loglevel ERROR
+
+deactivate
--- run_vikings.sh
+++ run_vikings.sh
@@ -1,2 +1,13 @@
+# create venv
+python3 -m venv venv
+
+# activate venv
+source venv/bin/activate
+
+# install requirements
+pip install -r requirements.txt
+
 curr_datetime=$(date +%Y_%m_%d-%H%M%S)
 scrapy crawl vikings -O log/vikings_output_"${curr_datetime}".json --logfile log/vikings_"${curr_datetime}".log --loglevel ERROR
+
+deactivate
